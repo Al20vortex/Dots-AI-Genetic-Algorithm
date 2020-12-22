0c205da63fbf832ea6f7a55e67323590634d8281
Alex Deaconu <al20vortex@gmail.com>
2020-12-22 15:55:27 -0700

    single dot moves perfectly!

--- dot.py
+++ dot.py
@@ -1,13 +1,25 @@
 
 
 class Dot:
-    def __init__(self, loc, acc):
+    def __init__(self, loc, vel, acc):
         self.loc = loc
+        self.vel = vel
         self.acc = acc
 
+    # moves dot to next position
     def move_dot(self):
-        self.loc[0] += self.acc[0]
-        self.loc[1] += self.acc[1]
+        self.vel[0] += self.acc[0]
+        self.vel[1] += self.acc[1]
+        if self.vel[0] > 5:
+            self.vel[0] = 5
+        if (self.vel[0] < -5):
+            self.vel[0] = -5
+        if (self.vel[1] > 5):
+            self.vel[1] = 5
+        if (self.vel[1] < -5):
+            self.vel[1] = -5
+        self.loc[0] += self.vel[0]
+        self.loc[1] += self.vel[1]
 
 
 
--- main.py
+++ main.py
@@ -10,9 +10,11 @@ width = 1200
 game_window = pyglet.window.Window(width, height)
 batch = pyglet.graphics.Batch()
 curr_instruction = 0
-d1 = Dot(np.array([width / 2, height / 8]), np.array([0, 0]))
+d1 = Dot(np.array([width / 2, height / 8]), np.array([0, 0]), np.array([0, 0]))
 dot1_brain = Brain(10000)
 circle = shapes.Circle(d1.loc[0], d1.loc[1], 4, color=(50, 225, 30), batch=batch)
+goal = shapes.Circle(width/2, height - 15, 12, color=(255,69,0), batch=batch)
+
 
 @game_window.event
 def on_draw():
@@ -34,5 +36,5 @@ if __name__ == '__main__':
     # for x in range(0, dot1_brain.size, 1):
     #     d1.acc = dot1_brain.dot_brain[x]
     #     d1.move_dot()
-    pyglet.clock.schedule_interval(update, 1/120.0)
+    pyglet.clock.schedule_interval(update, 1/30.0)
     pyglet.app.run()
